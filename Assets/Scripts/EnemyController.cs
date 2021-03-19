4ceb725b490142b39f71d39d6f5dae8d3f9a28d7
Brian Maher <bmaher91@gmail.com>
2021-03-19 06:41:58 -0600

    Add enemy controller and handle death

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Dependencies")] 
+    [SerializeField] private Damageable damageable;
+
+    private void Awake()
+    {
+        damageable.onDie.AddListener(() => Destroy(gameObject));
+    }
+}
