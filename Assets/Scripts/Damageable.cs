1b2387947d359d3a8e0eb28f852e04d46440d362
Brian Maher <bmaher91@gmail.com>
2021-03-18 21:53:36 -0600

    Flamethrower now kills the plant

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -3,11 +3,12 @@ using UnityEngine.Events;
 
 public class Damageable : MonoBehaviour
 {
-    [HideInInspector] public UnityEvent onDie;
-    
     [Header("Config")] 
     [SerializeField] private float health = 1;
 
+    [Header("Events")]
+    public UnityEvent onDie;
+    
     public void TakeDamage(float damage)
     {
         health -= damage;
--- Assets/Scripts/Fireable.cs
+++ Assets/Scripts/Fireable.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.VFX;
 
 public class Fireable : MonoBehaviour
@@ -6,7 +7,11 @@ public class Fireable : MonoBehaviour
     [Header("Dependencies")]
     [SerializeField] private VisualEffect flamethrowerEffect;
     [SerializeField] private AudioSource flamethrowerSoundEffect;
+    [SerializeField] private GameObject flameColliderPrefab;
+    [SerializeField] private Transform nozzleTransform;
 
+    private bool isFiring;
+    
     private void Awake()
     {
         flamethrowerEffect.Stop();
@@ -15,13 +20,31 @@ public class Fireable : MonoBehaviour
 
     public void StartFiring()
     {
+        isFiring = true;
         flamethrowerEffect.Play();
         flamethrowerSoundEffect.Play();
+        StartCoroutine(SpawnFlameColliders());
     }
 
     public void StopFiring()
     {
+        isFiring = false;
         flamethrowerEffect.Stop();
         flamethrowerSoundEffect.Stop();
+        StopCoroutine(SpawnFlameColliders());
+    }
+
+    private IEnumerator SpawnFlameColliders()
+    {
+        var flameCollider = Instantiate(flameColliderPrefab);
+        flameCollider.transform.position = nozzleTransform.position;
+        flameCollider.transform.rotation = nozzleTransform.rotation;
+        
+        yield return new WaitForSeconds(0.5f);
+        
+        if (isFiring)
+        {
+            yield return StartCoroutine(SpawnFlameColliders());
+        }
     }
 }
