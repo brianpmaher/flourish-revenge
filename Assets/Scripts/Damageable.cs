a30877d109a2cc7a3dfef9f44a314914a2ac1744
Brian Maher <bmaher91@gmail.com>
2021-03-19 06:42:56 -0600

    Hide onDie in inspector

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -3,12 +3,11 @@ using UnityEngine.Events;
 
 public class Damageable : MonoBehaviour
 {
+    [HideInInspector] public UnityEvent onDie;
+    
     [Header("Config")] 
     [SerializeField] private float health = 1;
 
-    [Header("Events")]
-    public UnityEvent onDie;
-    
     public void TakeDamage(float damage)
     {
         health -= damage;
