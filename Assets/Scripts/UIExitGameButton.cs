141fdd1574a4f610424186a0de19d1d667b5d8f9
Brian Maher <bmaher91@gmail.com>
2021-03-25 07:21:37 -0600

    Add exit button

---
+++ Assets/Scripts/UIExitGameButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UIExitGameButton : MonoBehaviour
+{
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(ExitGame);
+    }
+
+    private void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
